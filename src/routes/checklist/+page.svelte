bb8a6c4f519f9252f23476978a4097f89f50d324
brunosag <brunosag02@gmail.com>
2024-04-10 02:28:58 -0300

    chore: add checklist page for development

---
+++ src/routes/checklist/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import LucideCheck from '~icons/lucide/check';
+	import MdiDot from '~icons/mdi/dot';
+	import checklist from './checklist.json';
+</script>
+
+<div class="container my-4">
+	{#each Object.entries(checklist) as [title, tasks]}
+		<h2>{title}</h2>
+		<div class="mb-6">
+			{#each tasks as task}
+				{@const completed = task[1]}
+				<div class={cn(completed && 'bg-primary-solid/20', 'flex items-center gap-4 p-4')}>
+					<div class="h-5 w-5 shrink-0">
+						{#if completed}
+							<LucideCheck class="h-full w-full" />
+						{:else}
+							<MdiDot class="h-full w-full" />
+						{/if}
+					</div>
+					<span class="font-medium">{task[0]}</span>
+				</div>
+			{/each}
+		</div>
+	{/each}
+</div>
