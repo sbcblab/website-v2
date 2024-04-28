45aa9fda4a14342459f6c20f3b6769ca8a276dfe
brunosag <brunosag02@gmail.com>
2024-04-28 19:08:49 -0300

    style: redesign tools and datasets list

--- src/components/section/unique/unique-tools-datasets.svelte
+++ src/components/section/unique/unique-tools-datasets.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { ToolDataset } from '$lib/data';
+	import { onMount } from 'svelte';
 
 	export let component: {
 		tools: ToolDataset[];
@@ -7,20 +8,41 @@
 	};
 
 	const { tools, datasets } = component;
+
+	let cards: HTMLDivElement;
+
+	function normalizeHeight() {
+		const cardsArray = Array.from(cards.children) as HTMLDivElement[];
+
+		cardsArray.forEach((card) => (card.style.height = 'auto'));
+
+		const heights = cardsArray.map((card) => card.clientHeight);
+		const maxHeight = Math.max(...heights);
+
+		cardsArray.forEach((card) => (card.style.height = `${maxHeight}px`));
+	}
+
+	onMount(() => {
+		normalizeHeight();
+		window.addEventListener('resize', normalizeHeight);
+	});
 </script>
 
-<div class="container mb-16 grid grid-cols-2 gap-4 md:grid-cols-4">
+<div
+	bind:this={cards}
+	class="container mb-16 grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-4"
+>
 	{#each [...tools, ...datasets] as item}
-		<div class="flex flex-col gap-1 rounded-lg border p-4 text-sm">
-			<img src={item.imageUrl} alt={item.title} class="aspect-[5/4]" />
-			<strong>{item.title}</strong>
-			<span>{item.description}</span>
-			<a
-				href={item.link}
-				target="_blank"
-				class="mt-auto w-fit pt-4 font-medium text-primary transition-opacity hover:opacity-70"
-				>Read more</a
-			>
-		</div>
+		<a
+			href={item.link}
+			target="_blank"
+			class="flex items-center gap-8 rounded-lg border p-8 shadow-sm transition-all hover:scale-[1.02] hover:shadow-lg md:flex-col md:justify-center md:text-center"
+		>
+			<img src={item.imageUrl} alt={item.title} class="w-20 md:h-28 md:w-auto" />
+			<div class="flex flex-col gap-3">
+				<span class="text-lg font-semibold">{item.title}</span>
+				<span class="text-xs font-light text-foreground-paragraph">{item.description}</span>
+			</div>
+		</a>
 	{/each}
 </div>
