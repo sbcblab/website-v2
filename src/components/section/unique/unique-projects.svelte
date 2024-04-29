f36eb660c2df85a2b08a083bce9b97d9ff932f7f
brunosag <brunosag02@gmail.com>
2024-04-29 11:36:52 -0300

    style: redesign projects section list

--- src/components/section/unique/unique-projects.svelte
+++ src/components/section/unique/unique-projects.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Button } from '$components/ui/button';
 	import type { Projects } from '$lib/types';
 
 	export let component: Projects;
@@ -6,24 +7,32 @@
 	const { projects } = component;
 </script>
 
-<div class="container flex flex-col gap-8">
+<div class="container mt-20 flex flex-col gap-20 md:gap-10">
 	{#each projects as project}
-		<div class="space-y-4 border p-4">
-			<a href={`https://sbcb.inf.ufrgs.br${project.link}`} target="_blank" class="text-foreground">
-				<h3 class="text-xl/[1.25]">{project.title}</h3>
+		<div
+			class="flex flex-col items-center gap-8 rounded-lg text-center transition-shadow duration-200 md:flex-row md:p-8 md:text-start md:hover:shadow-lg"
+		>
+			<a href={`https://sbcb.inf.ufrgs.br${project.link}`} target="_blank" class="shrink-0">
+				<img
+					src={project.imageUrl}
+					alt={project.title}
+					class="aspect-square w-64 rounded-lg object-contain"
+				/>
 			</a>
-			<div class="flex gap-4">
-				<img src={project.imageUrl} alt={project.title} class="h-52 rounded-md" />
-				<div class="flex h-52 flex-col justify-between gap-4 text-sm/[1.5]">
-					<span class="overflow-hidden text-ellipsis whitespace-pre-line">
-						{project.description}
-					</span>
-					<a
-						href={`https://sbcb.inf.ufrgs.br${project.link}`}
-						target="_blank"
-						class="font-medium text-primary transition-opacity hover:opacity-70">Read more</a
-					>
-				</div>
+			<div class="flex flex-col items-center gap-8 md:items-start">
+				<a href={`https://sbcb.inf.ufrgs.br${project.link}`} target="_blank">
+					<h3 class="text-lg font-medium md:text-xl">{project.title}</h3>
+				</a>
+				<span class="text-sm font-light text-foreground-paragraph md:text-base"
+					>{project.description}</span
+				>
+				<Button
+					href={`https://sbcb.inf.ufrgs.br${project.link}`}
+					target="_blank"
+					class="rounded-full"
+				>
+					Read more
+				</Button>
 			</div>
 		</div>
 	{/each}
