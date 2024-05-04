78772ef82700e5d416989b72dc94fc6a5c3fb446
brunosag <brunosag02@gmail.com>
2024-05-04 06:05:35 -0300

    style: adjust members section style and responsiveness

--- src/components/section/unique/unique-members.svelte
+++ src/components/section/unique/unique-members.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import type { Members } from '$lib/types';
 	import { cn } from '$lib/utils';
-	import DblpIcon from '~icons/academicons/dblp';
-	import GoogleScholarIcon from '~icons/academicons/google-scholar';
-	import LattesIcon from '~icons/academicons/lattes';
-	import OrcidIcon from '~icons/academicons/orcid';
-	import ResearchGateIcon from '~icons/academicons/researchgate';
+	import DblpIcon from '~icons/academicons/dblp-square';
+	import GoogleScholarIcon from '~icons/academicons/google-scholar-square';
+	import LattesIcon from '~icons/academicons/lattes-square';
+	import OrcidIcon from '~icons/academicons/orcid-square';
+	import ResearchGateIcon from '~icons/academicons/researchgate-square';
 
 	export let component: Members;
 
@@ -23,23 +23,25 @@
 	];
 </script>
 
-<div class="container grid grid-cols-4 gap-x-4 gap-y-16 text-center md:gap-x-6 md:text-start">
+<div
+	class="container mt-12 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2 sm:gap-y-16 sm:text-center md:grid-cols-3 lg:grid-cols-4"
+>
 	{#each members as member}
-		<div
-			class="col-span-2 flex flex-col gap-4 text-sm last:odd:col-start-2 md:col-span-1 md:last:odd:col-start-auto"
-		>
+		<div class="flex items-center gap-5 sm:flex-col sm:items-stretch">
 			<img
 				src={member.pictureUrl}
 				alt={member.name}
-				class="aspect-square rounded-md object-cover"
+				class="aspect-square h-full max-w-[35%] rounded-md object-cover sm:max-w-none"
 			/>
-			<div class="flex flex-col gap-1">
-				<span class="text-[1.1875rem]">{member.name}</span>
-				<span class="font-medium text-primary">{member.role}</span>
-			</div>
-			<div class="flex grow flex-col justify-between gap-5">
-				<span class="font-medium text-foreground-paragraph">{member.areas.join(', ')}</span>
-				<div class="flex flex-wrap justify-center gap-2 text-[2rem] md:justify-start">
+			<div class="flex flex-col gap-5 sm:h-full">
+				<div class="flex flex-col gap-0.5">
+					<span class="text-lg/[1.2] font-medium sm:text-xl">{member.name}</span>
+					<span class="text-xs font-medium text-primary">{member.role}</span>
+				</div>
+				<span class="text-xs font-light text-foreground-paragraph sm:grow sm:text-sm"
+					>{member.areas.join(', ')}</span
+				>
+				<div class="flex gap-0.5 sm:justify-center">
 					{#each links as { slug, icon }}
 						<a
 							href={member[slug]}
@@ -49,7 +51,7 @@
 								member[slug] && 'hover:text-foreground-paragraph/75'
 							)}
 						>
-							<svelte:component this={icon} class="h-6 w-6" />
+							<svelte:component this={icon} class="h-7 w-7 sm:h-9 sm:w-9" />
 						</a>
 					{/each}
 				</div>
