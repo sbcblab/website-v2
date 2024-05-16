6e65532b605639417038a230b8d37162a5c51efd
brunosag <brunosag02@gmail.com>
2024-05-16 09:31:37 -0300

    style: change container size from `1200px` to `1060px`

--- src/components/renderers/link-renderer.svelte
+++ src/components/renderers/link-renderer.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	export let href: string;
 	export let title: string;
-	export let text: string;
-
-	console.log(href, title, text);
 </script>
 
 <a {href} {title} target="_blank">
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ const config = {
 			center: true,
 			padding: '1rem',
 			screens: {
-				'2xl': '1200px'
+				'2xl': '1060px'
 			}
 		},
 		extend: {
