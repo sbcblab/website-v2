b3a12e4cf3735d23f237bf8e812347637a85795f
brunosag <brunosag02@gmail.com>
2024-07-03 03:36:12 -0300

    style: make datasets component mobile-responsive

--- src/components/section/general/general-datasets.svelte
+++ src/components/section/general/general-datasets.svelte
@@ -13,17 +13,17 @@
 	$: filteredDatasets = component.data.filter((dataset: any) =>
 		dataset.gse.toLowerCase().includes(search.trim().toLowerCase())
 	);
-	function handleMouseEnter(e: any) {
+	function openDownload(e: any) {
 		e.target.nextElementSibling.setAttribute('data-open', 'true');
 	}
 
-	function handleMouseLeave(e: any) {
+	function closeDownload(e: any) {
 		e.target.children.item(1).setAttribute('data-open', 'false');
 	}
 </script>
 
 <div class="container flex flex-col gap-4">
-	<div class="flex max-w-64 items-center gap-3 rounded-sm bg-gray-100 pl-4">
+	<div class="flex md:max-w-64 items-center gap-3 rounded-sm bg-gray-100 pl-4">
 		<SearchIcon class="size-4 text-gray-400" />
 		<input
 			type="text"
@@ -36,9 +36,11 @@
 		<p class="text-gray-500">No matching datasets.</p>
 	{/if}
 	{#each filteredDatasets as dataset}
-		<div class="flex items-center gap-14 rounded-2xl bg-gray-100 px-14 py-10">
+		<div
+			class="flex flex-col items-center gap-10 rounded-2xl bg-gray-100 px-14 py-10 md:flex-row md:gap-14"
+		>
 			<div class="grow">
-				<div class="flex justify-between gap-10 border-b pb-4">
+				<div class="flex flex-wrap justify-between gap-8 border-b pb-4">
 					{#each component.displayedInfo as info}
 						<div class="flex flex-col gap-1">
 							<span class="text-xs font-bold uppercase text-gray-400">
@@ -48,7 +50,7 @@
 						</div>
 					{/each}
 				</div>
-				<div class="flex justify-between gap-10 pt-4">
+				<div class="flex flex-wrap justify-between gap-8 pt-4">
 					{#each Object.entries(dataset.scores) as [key, value]}
 						<div class="flex flex-col gap-1">
 							<span class="text-xs font-bold uppercase text-gray-400">{key}</span>
@@ -58,9 +60,9 @@
 				</div>
 			</div>
 			{#if component.downloadOptions}
-				<div class="group relative" on:pointerleave={handleMouseLeave}>
+				<button class="group relative text-start" on:mouseleave={closeDownload}>
 					<button
-						on:mouseenter={handleMouseEnter}
+						on:mouseenter={openDownload}
 						class="rounded-md bg-gray-200 p-3 text-gray-400 transition-colors group-hover:bg-gray-300 group-hover:text-gray-500"
 					>
 						<DownloadIcon class="size-5" />
@@ -82,7 +84,7 @@
 							</a>
 						{/each}
 					</div>
-				</div>
+				</button>
 			{/if}
 		</div>
 	{/each}
