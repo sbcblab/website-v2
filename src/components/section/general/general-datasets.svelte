d79e47252d1dc1650b17ceec635fa3b4e80a0a60
brunosag <brunosag02@gmail.com>
2024-07-04 04:51:21 -0300

    feat: add sort to datasets

--- src/components/section/general/general-datasets.svelte
+++ src/components/section/general/general-datasets.svelte
@@ -6,36 +6,58 @@
 	import { quintOut } from 'svelte/easing';
 	import { writable } from 'svelte/store';
 	import { fly } from 'svelte/transition';
+	import ArrowDownUpIcon from '~icons/lucide/arrow-down-up';
 	import FilterIcon from '~icons/lucide/filter';
 	import SearchIcon from '~icons/lucide/search';
+	import SortAscIcon from '~icons/lucide/sort-asc';
 
 	export let component: Datasets;
 
-	let filteredDatasets = component.data;
+	let datasets = component.data;
 
 	const gseFilter = writable<string | null>(null);
-	const typeFilter = writable<Selected<string | null>>({ value: null });
+	const typeFilter = writable<Selected<string | null>>({ value: null, label: 'All' });
+	const sort = writable<Selected<string>>({
+		value: component.sortOptions[0].jsonPropName,
+		label: component.sortOptions[0].label
+	});
+	const sortAscending = writable<boolean>(true);
 
-	gseFilter.subscribe(() => filterDatasets());
-	typeFilter.subscribe(() => filterDatasets());
+	gseFilter.subscribe(() => (datasets = sortDatasets(filterDatasets(datasets))));
+	typeFilter.subscribe(() => (datasets = sortDatasets(filterDatasets(datasets))));
+	sort.subscribe(() => (datasets = sortDatasets(datasets)));
+	sortAscending.subscribe(() => (datasets = sortDatasets(datasets)));
 
-	function filterDatasets() {
+	function filterDatasets(datasets: any[]) {
 		const typeFilterValue = $typeFilter.value;
-		let datasets = component.data;
+		let filteredDatasets = component.data;
 		if (typeFilterValue) {
-			datasets = datasets.filter((dataset: any) => dataset.type === typeFilterValue);
+			filteredDatasets = filteredDatasets.filter(
+				(dataset: any) => dataset.type === typeFilterValue
+			);
 		}
 		if ($gseFilter) {
-			datasets = datasets.filter((dataset: any) =>
+			filteredDatasets = filteredDatasets.filter((dataset: any) =>
 				dataset.gse.toLowerCase().includes(($gseFilter as string).trim().toLowerCase())
 			);
 		}
-		filteredDatasets = datasets;
+		return filteredDatasets;
+	}
+
+	function sortDatasets(datasets: any[]) {
+		const sortProp = $sort.value;
+		let sortedDatasets;
+		if ($sortAscending) {
+			sortedDatasets = datasets.sort((a, b) => parseInt(a[sortProp]) - parseInt(b[sortProp]));
+		} else {
+			sortedDatasets = datasets.sort((a, b) => parseInt(b[sortProp]) - parseInt(a[sortProp]));
+		}
+		return sortedDatasets;
 	}
 </script>
 
 <div class="container flex flex-col gap-4">
-	<div class="flex flex-col items-end justify-between gap-4 md:flex-row">
+	<div class="flex flex-col-reverse items-end justify-between gap-4 md:flex-row">
 		<div class="flex w-full items-center gap-3 rounded-sm bg-gray-100 pl-4 md:w-64">
 			<SearchIcon class="size-4 text-gray-400" />
 			<input
@@ -46,14 +68,14 @@
 			/>
 		</div>
 
-		{#if component.filterPropJsonName && component.filterPropLabel && component.filterOptions}
+		<div class="flex flex-col gap-3 max-md:w-full md:flex-row md:items-end md:gap-4">
 			<div class="flex items-end gap-2">
-				<div class="flex w-full flex-col gap-1 md:w-64">
+				<div class="flex w-full flex-col gap-1 md:w-56">
 					<span class="text-sm font-medium text-foreground">Cancer type</span>
 					<Select.Root bind:selected={$typeFilter}>
 						<Select.Trigger class="w-full">
 							<div class="flex items-center gap-2.5">
-								<FilterIcon class="size-4	 text-gray-400" />
+								<FilterIcon class="size-4 text-gray-400" />
 								<Select.Value placeholder="All" class="translate-y-[1px]" />
 							</div>
 						</Select.Trigger>
@@ -70,13 +92,43 @@
 					</Select.Root>
 				</div>
 			</div>
-		{/if}
+
+			<div class="flex items-end gap-2">
+				<div class="flex w-full flex-col gap-1 md:w-56">
+					<span class="text-sm font-medium text-foreground">Sort by</span>
+					<Select.Root bind:selected={$sort}>
+						<Select.Trigger class="w-full">
+							<div class="flex items-center gap-2.5">
+								<ArrowDownUpIcon class="size-4 text-gray-400" />
+								<Select.Value class="translate-y-[1px]" />
+							</div>
+						</Select.Trigger>
+						<Select.Content
+							transition={(el) => fly(el, { duration: 150, y: -4, easing: quintOut })}
+						>
+							{#each component.sortOptions as { jsonPropName, label }}
+								<Select.Item value={jsonPropName} {label}>
+									{label}
+								</Select.Item>
+							{/each}
+						</Select.Content>
+					</Select.Root>
+				</div>
+
+				<button
+					on:click={() => ($sortAscending = !$sortAscending)}
+					class="size-9 shrink-0 rounded-full p-2 text-gray-500 transition-colors hover:bg-gray-100"
+				>
+					<SortAscIcon class="size-full transition-transform {!$sortAscending && 'rotate-180'}" />
+				</button>
+			</div>
+		</div>
 	</div>
 
-	{#if filteredDatasets.length === 0}
+	{#if datasets.length === 0}
 		<p class="text-center text-gray-500">No matching datasets.</p>
 	{/if}
-	{#each filteredDatasets as dataset}
+	{#each datasets as dataset}
 		<div
 			class="flex flex-col items-center gap-10 rounded-2xl bg-gray-100 px-14 py-10 md:flex-row md:gap-14"
 		>
